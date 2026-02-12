254646a71a296114161d0d785bb1ce9648534366
saifaldin hossam <actually.saif@gmail.com>
2026-02-12 19:47:28 +0200

    hero section added, elements remaining

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import Navbar from '../Components/Layout/Navbar';
+import Hero from '../Components/Layout/Hero';
 
 const Home = () => {
     return ( 
+        <>
         <Navbar />
+        <Hero />
+        </>
      );
 }
  
