83639fd2c6b16ec6e75ffda1c4b77417faaac1eb
Samia Ahmed <23-101162@students.eui.edu.eg>
2026-02-13 19:10:43 +0200

    learn more section

---
+++ src/Components/Layout/learnmore.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './learnmore.css';
+
+const Learnmore = () => {
+    return ( 
+        <>
+        <div className='maindiv'>
+            <div className='img'>
+<div className='div2'>
+    <h3 className='div2 h3'>
+        CIB on an international scale
+    </h3>
+        <h4 className='div2 h4'>
+       Learn more about CIB's presence outside of Egypt
+    </h4>
+    <div className='learnmorebtn'>Learn more</div>
+</div>
+            </div>
+        </div>
+        </>
+     );
+}
+ 
+export default Learnmore;
