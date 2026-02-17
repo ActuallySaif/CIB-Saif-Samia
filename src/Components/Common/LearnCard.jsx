618ae8c42e081964eb5429413dad4c2195c286cc
ActuallySaif <actually.saif@gmail.com>
2026-02-17 10:44:40 +0200

    added props for learning center

--- src/Components/Common/LearnCard.jsx
+++ src/Components/Common/LearnCard.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './LearnCard.css'
-import LCImg from '../../Assets/LearnCard.png'
 import ReadMore from './ReadMore';
 
-const LearnCard = () => {
+const LearnCard = (props) => {
     return ( 
         <div className='LearnCard'>
-            <img src={LCImg} alt="" />
+            <img src={props.LCImg} alt="" />
             <div className='LCcontent'>
-                <p className='LCTitle'>Buying and making a home</p>
+                <p className='LCTitle'>{props.LCTitle}</p>
                 <p className='LCSub'>
-                    Buying a home can be an emotional process, but it's important to approach it logically
+                    {props.LCSub}
                 </p>
                 <ReadMore />
             </div>  
--- src/Components/Layout/LearnCenter.jsx
+++ src/Components/Layout/LearnCenter.jsx
@@ -4,22 +4,25 @@ import SecSub from '../Common/SecSub';
 import './LearnCenter.css'
 import LearnCard from '../Common/LearnCard';
 import Button from '../Common/Button';
+import LCImg1 from '../../Assets/LearnCard.png'
+import LCImg2 from '../../Assets/LearnCard2.jpg'
+import LCImg3 from '../../Assets/LearnCard3.png'
 
 const LearnCenter = () => {
     return ( 
         <section className='LearnCenter'>
             <div className='LCHeader'>
-                <SecSub />
-                <SecTitle />
+                <SecSub SSubText="Learning Center"/>
+                <SecTitle STitleText="Learn more about banking and your finances"/>
             </div>
 
             <div className='LearnCards'>
-                <LearnCard />
-                <LearnCard />
-                <LearnCard />
+                <LearnCard LCImg={LCImg1} LCTitle="Buying and making a home" LCSub="Buying a home can be an emotional process, but it's important to approach it logically"/>
+                <LearnCard LCImg={LCImg2} LCTitle="Traveling on a budget" LCSub="So for all of you travelers, take a vacation without spending a fortune with these helpful tips and tricks:"/>
+                <LearnCard LCImg={LCImg3} LCTitle="University life tips" LCSub="The impact of a good education can be transformative."/>
             </div>
 
-            <Button />
+            <Button MBtnText="View More"/>
         </section>
      );
 }
